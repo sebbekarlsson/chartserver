550b37e42c2934ecf817aaddb5485cdb6a5b741e
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-06-01 09:33:04 +0200

    better unit test for create_base64_filename

--- chartserver/tests/test_utils.py
+++ chartserver/tests/test_utils.py
@@ -1,8 +1,13 @@
 from chartserver.utils import create_base64_filename
+from base64 import b64encode
 
 
 def test_create_base64_filename():
-    assert len(create_base64_filename(['hello', 'world', '!', '!'])) > 8
+    assert create_base64_filename(['hello', 'world', '!', '!']) ==\
+        b64encode('hello') +\
+        b64encode('world') +\
+        b64encode('!') +\
+        b64encode('!') + '.png'
 
     assert '.png' in create_base64_filename(['hello', 'this', 'is', 'me'])
     assert '.jpg' in create_base64_filename(
